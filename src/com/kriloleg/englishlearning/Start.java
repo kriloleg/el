0c8817cdbb9ee93e1938bb32cd004c8f04ce3d96
unknown <oleh.kril@ods1-lhp-f36934.synapse.com>
2015-01-27 18:02:27 +0200

    Refactored code. Customize exceptions

--- src/com/kriloleg/englishlearning/Start.java
+++ src/com/kriloleg/englishlearning/Start.java
@@ -1,11 +1,9 @@
 package com.kriloleg.englishlearning;
 
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.Console;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Scanner;
 
 import com.kriloleg.englishlearning.loader.Loader;
 import com.kriloleg.englishlearning.parser.Parser;
@@ -13,14 +11,12 @@ import com.kriloleg.englishlearning.parser.Parser;
 public class Start {
     private Loader loader;
     private Parser parser;
-    private PrintStream out;
-    private InputStream in;
-    private Scanner scanner;
+    private Console console;
     
     public Start() {
         loader = new Loader();
         parser = new Parser();
-        out = System.out; 
+        console = System.console();
     }
     
     public static void main(String[] args) {
@@ -30,26 +26,21 @@ public class Start {
     }
     
     private void studyEnglish(Map<String, String> wordsMap) {
-        scanner = new Scanner(System.in);
         for(Entry<String, String> entry : wordsMap.entrySet()) {
-            out.println(entry.getKey());
-            String line = scanner.nextLine().trim();
+            String key = entry.getKey();
             
-            if (!wordsMap.containsValue(line)) {
-                out.println("Your word isn't correct!");
+            console.printf("%s: ", key);
+            String inputValue = console.readLine().trim();
+            
+            if (!wordsMap.containsValue(inputValue)) {
+                console.printf("Wrong! Correct word is: %s \n", wordsMap.get(key));
             }
         }    
     }
-    
+
     public Map<String, String> loadWords(String filename){
         List<String> lines = loader.load(filename);
 
         return parser.parse(lines);
-    }
-    
-    private void printOnScreen(Map<String, String> wordsMap){
-        for(Entry<String, String> entry : wordsMap.entrySet()) {
-            out.println(entry.getKey() + " = " + entry.getValue());
-        }
     }
 }
--- src/com/kriloleg/englishlearning/exceptions/FileNotFoundException.java
+++ src/com/kriloleg/englishlearning/exceptions/FileNotFoundException.java
@@ -1,10 +1,9 @@
 package com.kriloleg.englishlearning.exceptions;
 
 public class FileNotFoundException extends RuntimeException {
-    private String message;
 
     public FileNotFoundException(String message) {
-        this.message = message;
+        super(message);
     }
 
 }
